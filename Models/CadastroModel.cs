6287cf01f538ec0512860cc358410920204c4a73
fulvius10 <43765376+fulvius10@users.noreply.github.com>
2023-11-07 20:57:49 -0300

    Criação da CadastroModel.cs

---
+++ Models/CadastroModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TremBaum.Models
+{
+    public class CadastroModel
+    {
+        public String? emailCadastro { get; set; }
+        public String? passwordCadastro { get; set; }
+        public String? userNameCadastro { get; set; }
+
+    }
+}
