29a1c732deff4adeea703115923cc668fd99e365
Carlos Henrique Vilar de Almeida Malta <126994650+Carlosvilar99@users.noreply.github.com>
2023-11-07 21:01:01 -0300

    Criação CadastroController.cs

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TremBaum.Models;
+
+namespace TremBaum.Controllers
+{
+	public class CadastroController : Controller
+	{
+		public IActionResult Index(CadastroModel cadastromodel)
+		{
+			String usuarioCadastro = cadastromodel.userNameCadastro;
+			String senhaCadastro = cadastromodel.passwordCadastro;
+			String emailCadastro = cadastromodel.emailCadastro;
+
+			public static int ValidarUsuarioCadastro()
+			{
+				if (usuarioCadastro.Length > 15 && usuarioCadastro == null)
+				{
+					throw new ArgumentException("Usuario tem que ser maior do que 0 e menor ou igual a 15");
+				}
+
+				return ValidarUsuarioCadastro();
+			}
+			usuarioCadastro = "";
+			usuarioCadastro.ValidarUsuarioCadastro();
+
+
+
+
+
+
+
+			return View();
+		}
+	}
+}
